7c47e7023116d307cdedb456a72f78ca627c917e
baoth96 <thaihoangbao112@gmail.com>
2022-12-12 15:37:19 +0700

    project

---
+++ ecourses/courses/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='/uploads/%Y/%m')
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+
+class Course(models.Model):
+    subject = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=False, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
