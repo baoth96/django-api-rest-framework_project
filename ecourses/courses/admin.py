51f3f79634cf7bdf4179bac7a9c4bca7042d9382
baoth96 <thaihoangbao112@gmail.com>
2022-12-14 15:45:09 +0700

    project

--- ecourses/courses/admin.py
+++ ecourses/courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Course, Lesson, Tag
+
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Lesson)
+git
