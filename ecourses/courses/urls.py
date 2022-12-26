c44448375655df5cd9ab72e3dbfbfe6acddbbf6d
baoth96 <thaihoangbao112@gmail.com>
2022-12-26 16:01:22 +0700

    api

--- ecourses/courses/urls.py
+++ ecourses/courses/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('courses', views.CourseViewSet)
+router.register('lessons', views.LessonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- ecourses/courses/views.py
+++ ecourses/courses/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
-from .models import Course
-from .serializers import CourseSerializer
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -14,6 +16,22 @@ class CourseViewSet(viewsets.ModelViewSet):
             return [permissions.AllowAny()]
 
         return [permissions.IsAuthenticated()]
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    queryset = Lesson.objects.filter(active=True)
+    serializer_class = LessonSerializer
+
+    @action(methods=['post'], detail=True, url_path='hide_lesson', url_name='hide_lesson')
+    def hide_lesson(self, request, pk):
+        try:
+            l = Lesson.objects.get(pk=pk)
+            l.active = False
+            l.save()
+        except Lesson.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(data=LessonSerializer(l, context={'request': request}).data, status=status.HTTP_200_OK)
 
 
 def index(request):
